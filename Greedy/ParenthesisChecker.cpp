c383bcf708ace35227bcd07ad222e12532eef070
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-01-23 11:11:47 +0530

    Create ParenthesisChecker.cpp

---
+++ Greedy/ParenthesisChecker.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        stack<char> st;
+        for(int i=0;i<x.length();i++)
+        {
+            if(st.empty())
+            {
+            st.push(x[i]);
+                
+            }
+            else
+            {
+                if(x[i] == ')')
+                {
+                    if(st.top() == '(')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(x[i]);
+                    }
+                }
+                else if(x[i] == '}')
+                {
+                    if(st.top() == '{')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(x[i]);
+                    }
+                }
+                else if(x[i] == ']')
+                {
+                    if(st.top() == '[')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        st.push(x[i]);
+                    }
+                }
+                else 
+                {
+                    st.push(x[i]);
+                }
+            }
+        }
+        if(st.empty())
+            return true;
+        else 
+            return false;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
