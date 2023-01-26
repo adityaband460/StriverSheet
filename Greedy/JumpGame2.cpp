e0ad1c785754f6a6be33d440d3ae4be909f456cd
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-01-26 09:24:25 +0530

    Update JumpGame2.cpp

--- Greedy/JumpGame2.cpp
+++ Greedy/JumpGame2.cpp
@@ -52,6 +52,51 @@ class Solution{
         
     }
 };
+// Another Solution given in gfg
+
+    int minJumps(int arr[], int n){
+            // Your code here
+
+            if(n<=1)
+                return 0;
+            if(arr[0] ==0)
+                return -1;
+
+            // choose next jumptarget based on who has greater i + arr[i]
+
+            int maxReach = arr[0];// max how much we can travel
+            int steps = arr[0]; // consider steps as fuel
+            int jumps = 1; // our ans
+            for(int i=1;i<n;i++)
+            {
+                // if we reach to last pos then return jumps
+                if(i== n-1)
+                    return jumps;
+
+                // update maxReach everytime each pos have chance to update maxReach    
+                maxReach = max(maxReach,i+arr[i]);
+                // if we travel 1 index our fuel gets depleted by 1
+                steps--;
+
+                // if fuel becomes 0 , we can refill it by the 
+                // difference between i and maxReach as that much
+                // we can still travel
+                if(steps == 0)
+                {
+                    //we needed to jump as prev arr[i] fuel is over
+                    jumps++;
+                    if(i == maxReach) // i >= maxReach will also work
+                    {
+                        return -1;
+                    }
+
+                    // refill
+                    steps = maxReach - i;
+                }
+            }
+            return -1;
+
+        }
 
 
 //{ Driver Code Starts.
