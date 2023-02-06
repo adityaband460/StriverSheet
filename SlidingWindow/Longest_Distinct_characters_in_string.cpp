77e82abd43f2d424032d13bfe5eb3225a46ffea7
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-02-06 16:28:20 +0530

    easy 1 edge case

---
+++ SlidingWindow/Longest_Distinct_characters_in_string.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int longestSubstrDistinctChars (string S);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+
+        cout << longestSubstrDistinctChars (S) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+int longestSubstrDistinctChars (string S)
+{
+    // your code here
+    int n = S.length();
+    int i = 0,j = 0;
+    int maxlen = 0;
+    unordered_map<char,int>freq;
+    while( j < n)
+    {
+        if(freq.find(S[j]) == freq.end() )
+        {
+            maxlen = max(maxlen,j-i+1);
+            freq[S[j]] = 1;
+           // cout<<S[j]<<" inserted\n";
+            //cout<<"maxlen = "<<maxlen<<"\n";
+        }
+        else
+        {
+            freq[S[j]]++;
+            while(freq[S[j]] != 1)
+            {
+                freq[S[i]]--;
+                
+                // imp step if frequency of a char becomes 0 then remove it
+                // so that it can again be added as freq 1
+                
+                if(freq[S[i]] == 0)
+                    freq.erase(S[i]);
+                
+              //  cout<<S[i]<<" erased\n";
+                i++;
+            }
+        }
+
+        j++;
+    }
+    return maxlen;
+}
