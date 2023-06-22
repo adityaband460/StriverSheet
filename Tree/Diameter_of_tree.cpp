fd1a0244ec80ab189b72bfaaee2538d4b8cbdfd7
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-06-22 22:39:24 +0530

    Update Diameter_of_tree.cpp

--- Tree/Diameter_of_tree.cpp
+++ Tree/Diameter_of_tree.cpp
@@ -1,9 +1,31 @@
+
+// Approach 2 : O(N) : while calculating height calculate diameter and pass by parameter
+ // Function to return the diameter of a Binary Tree.
+    int heightModified(Node* root,int &maxi)
+    {
+        if(root == NULL)return 0;
+        
+        int lh = heightModified(root->left,maxi);
+        int rh = heightModified(root->right,maxi);
+        
+        maxi = max(maxi, lh+rh+1);
+        return max(lh,rh)+1;
+    }
+    int diameter(Node* root) {
+        int ans = 0;
+        heightModified(root,ans);
+        return ans;
+    }
+
+
+------------------------------------------------------------------------------------------------------------------------------
 int height(Node* root)
     {
         if(root == NULL)return 0;
         
         return max(height(root->left),height(root->right))+1;
     }
+///   Approach 1; O(N^2) time complexity  : for each node height calculated
     int diameter(Node* root) {
         if(root== NULL) return 0;
         
