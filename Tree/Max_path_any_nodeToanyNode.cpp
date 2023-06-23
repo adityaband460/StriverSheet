e570554e35575a6cf826a6b646d89f6a27c2a884
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-06-23 12:24:10 +0530

    Out of box thinking --- Max_path_any_nodeToanyNode.cpp

--- Tree/Max_path_any_nodeToanyNode.cpp
+++ Tree/Max_path_any_nodeToanyNode.cpp
@@ -1,7 +1,7 @@
  int findMax(Node*root ,int &maxi)
     {
         if(root== NULL) return 0;
-        
+         
         int lsum = max(0,findMax(root->left,maxi));
         int rsum = max(0,findMax(root->right,maxi));
         // actual ans
