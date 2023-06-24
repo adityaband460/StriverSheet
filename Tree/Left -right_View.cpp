8a15c655bcf064d741a03fcb9b8068e6e5c18b18
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-06-24 15:15:35 +0530

    preOrder-Left View , revPreorder-Right view

--- Tree/Left -right_View.cpp
+++ Tree/Left -right_View.cpp
@@ -18,3 +18,18 @@ vector<int> leftView(Node *root)
     preModified(root,0,ans);
     return ans;
 }
+//Function to return list containing elements of right view of binary tree.
+    void revPreOrder(Node *root,int lvl,vector<int>&ans)
+    {
+        if(root == NULL)return;
+        if(lvl==ans.size()) ans.push_back(root->data);
+        
+        revPreOrder(root->right,lvl+1,ans);
+        revPreOrder(root->left,lvl+1,ans);
+    }
+    vector<int> rightView(Node *root)
+    {
+       vector<int> ans;
+       revPreOrder(root,0,ans);
+       return ans;
+    }
