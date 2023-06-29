c77585754e61e7887fc95ec754840e35852e8d33
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-06-29 08:15:43 +0530

    Create Build_From_preorder.cpp

---
+++ BST/Build_From_preorder.cpp
@@ -0,0 +1,23 @@
+    //Function that constructs BST from its preorder traversal.
+    Node* BuildBSTfromPre(int pre[],int &i,int size,int maxBoundary)
+    {
+        //base case
+        if(i >= size) return NULL;
+        
+        if(pre[i] >= maxBoundary)
+            return NULL;
+            
+        Node* root = newNode(pre[i]);
+        i++;
+        root->left = BuildBSTfromPre(pre,i,size,root->data);
+        
+        root->right = BuildBSTfromPre(pre,i,size,maxBoundary);
+        
+        return root;
+    }
+    Node* post_order(int pre[], int size)
+    {
+        int a= 0;
+        return BuildBSTfromPre(pre,a,size,INT_MAX);
+        
+    }
