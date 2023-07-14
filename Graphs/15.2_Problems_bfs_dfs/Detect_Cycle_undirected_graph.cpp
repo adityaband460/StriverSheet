6140fe33c96487d0b1948f2290d9406659b2f84e
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-07-14 16:05:32 +0530

    Create Detect_Cycle_undirected_graph.cpp

---
+++ Graphs/15.2_Problems_bfs_dfs/Detect_Cycle_undirected_graph.cpp
@@ -0,0 +1,56 @@
+bool bfs(int node,int par,vector<int>&vis,vector<int>adj[])
+    {
+        queue<pair<int,int>>q;
+        q.push({node,par});
+        vis[node] = 1;
+        
+        while(!q.empty())
+        {
+            int vertex = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            for(int nbr : adj[vertex])
+            {
+                if(vis[nbr] == 0)
+                {
+                    vis[nbr] = 1;
+                    q.push({nbr,vertex});
+                }
+                else if(nbr != parent)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool dfs(int node,int par,vector<int>&vis,vector<int>adj[])
+    {
+        vis[node] = 1;
+        
+        for(int nbr : adj[node])
+        {
+            if(vis[nbr]==0)
+            {
+                if(dfs(nbr,node,vis,adj) == true)
+                    return true;
+            }
+            else if(nbr != par)//alredy visited but its not parent
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        
+        vector<int> vis(V,0);
+         for(int i=0;i<V;i++)
+         {
+             if(vis[i]==0)
+             {
+                 if(bfs(i,-1,vis,adj)==true) return true;
+             }
+         }
+         return false;
+    }
