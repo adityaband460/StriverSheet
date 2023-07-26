6cfa4446fe1288b823ce1b88a256ad260aa73a9c
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-07-26 22:15:47 +0530

    Update knapsack.cpp

--- Dyanamic Programming/knapsack/knapsack.cpp
+++ Dyanamic Programming/knapsack/knapsack.cpp
@@ -1,98 +1,100 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to return max value that can be put in knapsack of capacity W.
-    
-    int kSack(int W, int wt[], int val[], int n, int table[][1002])
+    int solveSuperSaiyan(int n,int cap ,int wt[],int val[],vector<vector<int>>&dp)
     {
-        // base case
-      if(n==0 || W==0)
-        return 0;
+        vector<int>cur(cap+1,0);
+        //base case will be covered as all are 0's
        
-      if(table[n][W] != -1)
-            return table[n][W];
-      // recursive case
-      if(wt[n-1] <= W)// we can have 2 choices
-      {
-          return table[n][W] = max(
-                (val[n-1]+ kSack(W-wt[n-1],wt,val,n-1,table)) ,
-                kSack(W,wt,val,n-1,table) 
-                );
-      }
-      else if( wt[n-1] > W)
-      {
-          return table[n][W] =  kSack(W,wt,val,n-1,table);
-      }
+       for(int size = 1;size<n+1;size++)
+       {
+           for(int capacity = cap;capacity>=1;capacity--)
+           {
+            int pick=0,notpick=0;
+            //pick : value of that item added but cap decreases
+            if(wt[size-1] <= capacity)
+            {
+                pick = val[size-1] + cur[capacity-wt[size-1]];
+            }
+            // notpick : value not added ,cap remains same
+            notpick = cur[capacity];
+            cur[capacity] = max(pick,notpick);
+           }
+           
+       }
+        return cur[cap];
+    }
+    
+    int solveSpaceOpti(int n,int cap ,int wt[],int val[],vector<vector<int>>&dp)
+    {
+        vector<int>prev(cap+1,0);
+        vector<int>cur(cap+1,0);
+        //base case will be covered as all are 0's
+       
+       for(int size = 1;size<n+1;size++)
+       {
+           for(int capacity = 1;capacity<cap+1;capacity++)
+           {
+            int pick=0,notpick=0;
+            //pick : value of that item added but cap decreases
+            if(wt[size-1] <= capacity)
+            {
+                pick = val[size-1] + prev[capacity-wt[size-1]];
+            }
+            // notpick : value not added ,cap remains same
+            notpick = prev[capacity];
+            cur[capacity] = max(pick,notpick);
+           }
+           prev = cur;
+       }
+        return prev[cap];
+    }
+    //Function to return max value that can be put in knapsack of capacity W.
+    int solveTabulation(int n,int cap ,int wt[],int val[],vector<vector<int>>&dp)
+    {
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<cap+1;j++)
+            {
+                dp[i][j] = 0;
+            }
+        }
+        //base case will be covered as all are 0's
+       
+       for(int size = 1;size<n+1;size++)
+       {
+           for(int capacity = 1;capacity<cap+1;capacity++)
+           {
+            int pick=0,notpick=0;
+            //pick : value of that item added but cap decreases
+            if(wt[size-1] <= capacity)
+            {
+                pick = val[size-1] + dp[size-1][capacity-wt[size-1]];
+            }
+            // notpick : value not added ,cap remains same
+            notpick = dp[size-1][capacity];
+            dp[size][capacity] = max(pick,notpick);
+           }
+       }
+        return dp[n][cap];
+    }
+    int solve(int n,int cap ,int wt[],int val[],vector<vector<int>>&dp)
+    {
+        //base case
+        if(n == 0 || cap == 0)
+        {
+            return 0;
+        }
+        if(dp[n][cap] != -1) return dp[n][cap];
+        int pick=0,notpick=0;
+        //pick : value of that item added but cap decreases
+        if(wt[n-1] <= cap)
+        {
+            pick = val[n-1] + solve(n-1,cap-wt[n-1],wt,val,dp);
+        }
+        // notpick : value not added ,cap remains same
+        notpick = solve(n-1,cap,wt,val,dp);
+        return dp[n][cap] = max(pick,notpick);
     }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-        int table[1002][1002];// n X W
-        memset(table,-1,sizeof(table));
-    
-    
-        for(int i=0;i<=W;i++)
-        {
-            table[0][i] = 0;
-        }
-        for(int i=0;i<=n;i++)
-        {
-            table[i][0] = 0;
-        }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=W;j++)
-            {
-                if(wt[i-1] <= j)// 
-                {
-                    table[i][j] = max( val[i-1] + table[i-1][j - wt[i-1]] , 
-                                        table[i-1][j]);
-                }
-                else
-                {
-                    table[i][j] = table[i-1][j];
-                }
-            }
-        }
-        return table[n][W];
-      //   return kSack(W,wt,val,n,table);
-       
+        vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
+        return solveSuperSaiyan(n,W,wt,val,dp);
     }
-};
-
-//{ Driver Code Starts.
-
-int main()
- {
-    //taking total testcases
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        //reading number of elements and weight
-        int n, w;
-        cin>>n>>w;
-        
-        int val[n];
-        int wt[n];
-        
-        //inserting the values
-        for(int i=0;i<n;i++)
-            cin>>val[i];
-        
-        //inserting the weights
-        for(int i=0;i<n;i++)
-            cin>>wt[i];
-        Solution ob;
-        //calling method knapSack()
-        cout<<ob.knapSack(w, wt, val, n)<<endl;
-        
-    }
-	return 0;
-}
-// } Driver Code Ends
