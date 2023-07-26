8c46d048808431e8cca96984e8df65b43af451b8
adityaband460 <65974661+adityaband460@users.noreply.github.com>
2023-07-26 17:26:37 +0530

    Update countSubsetGivenDifference.cpp

--- Dyanamic Programming/knapsack/countSubsetGivenDifference.cpp
+++ Dyanamic Programming/knapsack/countSubsetGivenDifference.cpp
@@ -1,88 +1,98 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
   public:
     int mod = 1e9+7;
-    int countSubsetSum(int n ,int sum,vector<vector<int>>&table,vector<int>& arr)
+    
+    //space optimization
+    int spaceOpti(int n,int sum,vector<int>&arr,
+    vector<vector<int>>&dp)
     {
-       if(n == 0)
+        vector<int>prev(sum+1,0);
+        //base case
+        prev[0] = 1;
+        // copy recurrence
+        for(int ind = 1;ind<n+1;ind++)
         {
-            if(sum == 0)
+            vector<int>cur(sum+1,0);
+            for(int target = 0;target <sum+1;target++)
+            {
+                int pick = 0,notpick=0;
+                if(arr[ind-1] <= target)
+                {
+                    pick = prev[target-arr[ind-1]];
+                }
+                notpick = prev[target];
+                cur[target] = (pick + notpick)%mod;
+            }
+            prev = cur;
+        }
+        return prev[sum];
+    }
+    int countSubsetSumTabulation(int n,int sum,vector<int>&arr,
+    vector<vector<int>>&dp)
+    {
+        //tabulation
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                dp[i][j] = 0;
+            }
+        }
+        //base case
+        dp[0][0] = 1;
+        // copy recurrence
+        for(int ind = 1;ind<n+1;ind++)
+        {
+            for(int target = 0;target <sum+1;target++)
+            {
+                int pick = 0,notpick=0;
+                if(arr[ind-1] <= target)
+                {
+                    pick = dp[ind-1][target-arr[ind-1]];
+                }
+                notpick = dp[ind-1][target];
+                dp[ind][target] = (pick + notpick)%mod;
+            }
+        }
+        return dp[n][sum];
+    }
+    int countSubsetSum(int n,int target,vector<int>&arr,vector<vector<int>>&dp)
+    {
+        // memoization
+        if(n==0)
+        {
+            if(target==0) 
                 return 1;
-            if(sum != 0)
+            else 
                 return 0;
         }
-        if(table[n][sum] != -1)
+        if(dp[n][target] != -1) return dp[n][target];
+        int pick = 0,notpick=0;
+        if(arr[n-1] <= target)
         {
-            return table[n][sum];
+            pick = countSubsetSum(n-1,target-arr[n-1],arr,dp);
         }
+        notpick = countSubsetSum(n-1,target,arr,dp);
+        return dp[n][target] = (pick + notpick)%mod;
         
-        if(arr[n-1] <= sum)
-        {
-            return table[n][sum] = (countSubsetSum(n-1,sum-arr[n-1],table,arr) + 
-                    countSubsetSum(n-1,sum,table,arr) )%mod;
-        }
-        else
-        {
-            return table[n][sum] = countSubsetSum(n-1,sum,table,arr);
-        }
+        
         
     }
-    
+  
     int countPartitions(int n, int d, vector<int>& arr) {
-       int sum =0;
-       for(int i=0;i<n;i++)
-       {
-            sum += arr[i];
-       }
-       // problem has no solution when
-    //   sum-d cant be divided into 2 equal parts so we get diff= d by adding d
-    //   to first subset
-    //  {s2} - {s1} = 0 , sum s2 + sum s1 = Sum of arr - d
-    // then just add remaining d sum to s2
-    
-    //  also no solution when difference is greater than sum of arr
-    // as max diff possible is sum of arr only
-     
-       if((sum - d)%2 != 0 )
+        int sum = 0;
+        for(int i=0;i<n;i++)
         {
-            return 0;
+            sum += arr[i];
         }
-        // sum-d should lie between 0 and 2*sum
-        int val = sum-d;
-        if( !(val <= 2*sum && val >= 0) )
-            return 0;
-        // else do the calculations
-          vector<vector<int>> table(n+1,vector<int>(sum+1,-1));
-          int partSumS1 = (sum - d )/2;
-          // cout<<sum<<"  <--\n";
-          return countSubsetSum(n,partSumS1,table,arr);
+        int target;
         
-      
+        if(sum - d >=0 && (sum-d)%2==0)
+            target = (sum-d)/2;
+        else
+            return 0;
+            
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        return spaceOpti(n,target,arr,dp);
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, d;
-        cin >> n >> d;
-        vector<int> arr;
-
-        for (int i = 0; i < n; ++i) {
-            int x;
-            cin >> x;
-            arr.push_back(x);
-        }
-
-        Solution obj;
-        cout << obj.countPartitions(n, d, arr) << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
